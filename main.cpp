1153ca2f32c1664712e9f2ee027fdf399d4897d6
vurdolag <48510944+vurdolag@users.noreply.github.com>
2021-10-21 00:15:17 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -185,8 +185,93 @@ void foo(void * arg) {
     loop->add(foo, arg, 5);
 }
 
+#include <Windows.h>
+#include <tlhelp32.h>
 
-int main() {
+
+DWORD GetProcessByExeName(char *ExeName)
+{
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+    if( hProcessSnap == INVALID_HANDLE_VALUE) {
+        cout << "Error GetProcessByExeName" << endl;
+        return false;
+    }
+
+    if (Process32First(hProcessSnap, &pe32))
+    {
+        do
+        {
+            if (!strcmpi(pe32.szExeFile, ExeName)) {
+                CloseHandle(hProcessSnap);
+                return pe32.th32ProcessID;
+            }
+        } while (Process32Next(hProcessSnap, &pe32));
+    }
+
+    CloseHandle(hProcessSnap);
+    return 0;
+}
+
+
+void check(char * name) {
+    DWORD PID = GetProcessByExeName(name);
+    if (!PID) {
+        cout << "Has not proc: " << name << endl;
+    }
+
+    else {
+        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, PID);
+        if (TerminateProcess(hProcess, 0)) {
+            cout << "is kill: " << name << endl;
+        }
+        else {
+            cout << "is not kill: " << name << endl;
+        }
+    }
+}
+
+void start(char * command) {
+    system(command);
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+    for (int i = 0; i < argc; i++) {
+        cout << argv[i] << endl;
+    }
+
+    if (argc < 4 || argc > 4) {
+        cout << "error: need 3 arg" << endl;
+        return 1;
+    }
+
+    char * name = argv[1];
+    char * command = argv[2];
+    int t = atoi(argv[3]);
+
+    //char * name = "notepad.exe";
+    //char * command = "%windir%\\system32\\notepad.exe";
+    while(1) {
+        thread(start, command).detach();;
+
+        Sleep(1000);
+
+        keybd_event(VK_RETURN, 0, 0, 0);
+        keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+
+        cout << "Start! " << name << endl;
+        Sleep(t);
+        check(name);
+        Sleep(1000);
+    }
+
+    return 0;
+
     auto loop = Loop(1024);
 
     for (int i = 0; i < 100; i++) {
