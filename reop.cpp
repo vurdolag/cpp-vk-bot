f5dc89b2f8c5e94a25abbdbab21d5a7feaa9090c
vurdolag <48510944+vurdolag@users.noreply.github.com>
2021-10-25 18:18:28 +0300

    Add files via upload

--- reop.cpp
+++ reop.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+struct RES {
+    HWND w;
+    const char * k;
+};
+
+
 DWORD GetProcessByExeName(char * ExeName)
 {
     PROCESSENTRY32 pe32;
@@ -32,7 +38,6 @@ DWORD GetProcessByExeName(char * ExeName)
     return 0;
 }
 
-
 void check(char * name) {
     DWORD PID = GetProcessByExeName(name);
     if (!PID) {
@@ -55,37 +60,104 @@ void start(char * command) {
     system(command);
 }
 
+void activate(HWND hwnd) {
+    Sleep(1000);
+    cout << "activate" << endl;
+    SetFocus(hwnd);
+    //GetWindow(hwnd, GW_OWNER);
+    //IsWindowVisible(hwnd);
+    Sleep(1000);
+}
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
+    auto r = (RES*)lParam;
+
+    WCHAR str[32];
+
+    auto b = GetWindowTextW(hwnd, str, 32);
+    if (!b) {
+        return 1;
+    }
+
+    printf("%ls\n", str);
+
+    wstring v = wstring(str);
+
+    WCHAR pat[32];
+
+    auto ptr = r->k;
+    for (int i = 0; i < 32; i++) {
+        pat[i] = ptr[i];
+    }
+
+    wstring p = pat;
+
+    int x = v.find(p);
+
+    printf("%i : %ls : %ls\n\n", x, pat, v.c_str());
+
+    if (x != -1) {
+        printf("Find -> %i : %ls : %ls\n\n", x, pat, v.c_str());
+        r->w = hwnd;
+        activate(hwnd);
+        return 0;
+    }
+
+    //MessageBoxW(nullptr, str,L"text",0)
+
+    Sleep(50);
+    return 1;
+}
+
 
 int main(int argc, char *argv[]) {
-
-    for (int i = 0; i < argc; i++) {
-        cout << argv[i] << endl;
-    }
+    //for (int i = 0; i < argc; i++) {
+    //    cout << argv[i] << endl;
+   // }
 
     int t = atoi(argv[1]) * 1000;
-    char * name = argv[2];
+    char * pat1 = argv[2];
+    char * pat2 = argv[3];
+    char * name = argv[4];
+    string command = string(argv[5]);
 
-    string command = string(argv[3]);
-
-    for (int i = 4; i < argc; i++) {
+    for (int i = 6; i < argc; i++) {
         command += " " + string(argv[i]);
     }
-
     command = "\"" + command + "\"";
-
     char * command_c = (char *)command.c_str();
 
-    //char * name = "notepad.exe";
-    //char * command = "%windir%\\system32\\notepad.exe";
+    RES * w = new RES;
+
     while(true) {
         cout << "Start! " << name << " " << command_c << endl;
-        thread(start, command_c).detach();;
+        thread(start, command_c).detach();
 
-        Sleep(1000 * 10);
+        Sleep(1000 * 13);
 
+        w->k = pat1;
+        EnumWindows(EnumWindowsProc, (LPARAM)w);
+
+        Sleep(1000);
         cout << "Click! " << name << endl;
         keybd_event(VK_RETURN, 0, 0, 0);
         keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+
+        Sleep(1000);
+
+        w->k = pat2;
+        EnumWindows(EnumWindowsProc, (LPARAM)w);
+
+
+        keybd_event(VK_LWIN, 0, 0, 0);
+        Sleep(300);
+        keybd_event(VK_UP, 0, 0, 0);
+
+
+        Sleep(300);
+        keybd_event(VK_UP, 0, KEYEVENTF_KEYUP, 0);
+        keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
+
 
         Sleep(t);
         check(name);
